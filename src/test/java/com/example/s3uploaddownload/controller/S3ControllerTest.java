59514a0de266a03b2ee3b9db5cb9dc352c8321c8
dev-hjkim <dev--test@naver.com>
2022-02-23 02:07:06 +0900

    220223, make s3 upload test code

---
+++ src/test/java/com/example/s3uploaddownload/controller/S3ControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.s3uploaddownload.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class S3ControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testUploadToAWS() {
+        MockMultipartFile file = null;
+        try {
+            file = new MockMultipartFile(
+                    "file",
+                    "Changdeokgung_korea.jpg",
+                    "img/jpeg",
+                    getClass().getClassLoader().getResourceAsStream("Changdeokgung_korea.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mvc.perform(multipart("/upload")
+                    .file(file))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
