84aff6a735d58a2dcbb983fbb3857954ff6d668a
dev-hjkim <dev--test@naver.com>
2022-02-23 02:13:17 +0900

    220223, remove unnecessary import

--- src/test/java/com/example/s3uploaddownload/controller/S3ControllerTest.java
+++ src/test/java/com/example/s3uploaddownload/controller/S3ControllerTest.java
@@ -1,21 +1,15 @@
 package com.example.s3uploaddownload.controller;
 
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
